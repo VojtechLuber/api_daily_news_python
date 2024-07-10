fccacf15cef279838387519fdebd9af216d9265c
VojtechLuber <vojtaluber@seznam.cz>
2024-07-10 13:08:39 +0200

    add links, specify language, limit news , add subject but error on line 18 because type and in future fix

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from  send_email import send_email
 
+topic = "tesla"
+
 api_key = "18b8baff3ebc4e7a91fd3ab62c435818"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
       "from=2024-06-10&sortBy=publishedAt&apiKey=" \
-      "18b8baff3ebc4e7a91fd3ab62c435818"
+      "18b8baff3ebc4e7a91fd3ab62c435818&" \
+      "language=en"
 
 
 # make request
@@ -15,9 +19,11 @@ content = request.json()
 
 # Access the article title and description
 body = ""
-for article in content ["articles"]:
+for article in content ["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: Todays News" + body + article["title"] + "\n"\
+               + article["description"] \
+                + "\n" + article[url] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
